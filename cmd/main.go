58eda1a0d388069749df9b23dbb4d8b3904ddbe3
Numbone <nurlybekqwerty@gmail.com>
2025-06-15 04:46:20 +0500

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
-package cmd
+package main
+
+import (
+	golang_todo_app "github.com/Numbone/golang-todo-app"
+	"github.com/Numbone/golang-todo-app/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(golang_todo_app.Server)
+	if err := srv.Run(":8081", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
--- server.go
+++ server.go
@@ -1,1 +1,27 @@
 package golang_todo_app
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   5 * time.Second,
+		Handler:        handler,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
