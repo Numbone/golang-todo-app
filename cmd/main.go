404ec21bb920abc42fc0b33dad00be09cc796741
Numbone <nurlybekqwerty@gmail.com>
2025-06-16 06:04:38 +0500

    add dependency

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	golang_todo_app "github.com/Numbone/golang-todo-app"
 	"github.com/Numbone/golang-todo-app/pkg/handler"
+	"github.com/Numbone/golang-todo-app/pkg/repository"
+	"github.com/Numbone/golang-todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(golang_todo_app.Server)
 	if err := srv.Run(":8081", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
