519513f04933df3a5dde6a71bb6783c59d6e3ea3
Numbone <nurlybekqwerty@gmail.com>
2025-06-18 05:21:46 +0500

    add migration with golang migration and up docker exec connect psql

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,25 @@ import (
 	"github.com/Numbone/golang-todo-app/pkg/handler"
 	"github.com/Numbone/golang-todo-app/pkg/repository"
 	"github.com/Numbone/golang-todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatal("init config err:", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(golang_todo_app.Server)
-	if err := srv.Run(":8081", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString(":8081"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
