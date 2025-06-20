5d70903025a55fb97141f8ae43e7be18dc290c5a
Numbone <nurlybekqwerty@gmail.com>
2025-06-21 04:38:01 +0500

    add env and viper ,godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	golang_todo_app "github.com/Numbone/golang-todo-app"
+	"github.com/Numbone/golang-todo-app"
 	"github.com/Numbone/golang-todo-app/pkg/handler"
 	"github.com/Numbone/golang-todo-app/pkg/repository"
 	"github.com/Numbone/golang-todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatal("init config err:", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("load .env file err:", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatal("init db err:", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(golang_todo_app.Server)
-	if err := srv.Run(viper.GetString(":8081"), handlers.InitRoutes()); err != nil {
+	if err := srv.Run(":"+viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
